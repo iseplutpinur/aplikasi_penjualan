dc2e13ec2f6eeb5a351c783e1fdb2110d9d82191
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-01-10 14:38:17 +0700

    Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Komponen halaman registrasi
+import Registrasi from "./pages/registrasi";
+import Login from "./pages/login";
+import LupaPassword from "./pages/lupa-password";
+import NotFound from "./pages/404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/registrasi" component={Registrasi} />
+        <Route path="/login" component={Login} />
+        <Route path="/lupa-password" component={LupaPassword} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/registrasi/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Registrasi() {
+    return <h1>Halaman Registrasi</h1>
+}
+
+export default Registrasi;
